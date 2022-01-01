3697ff728c34332d80f5b1402052489a765a71ce
Nick Chaloult <npchaloult@gmail.com>
2022-01-01 18:58:50 -0500

    Add wait_for_server_to_spin_up() helper func

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::error;
 use std::io::{BufRead, BufReader, Error, ErrorKind, Write};
-use std::process::{Command, Stdio};
+use std::process::{ChildStdout, Command, Stdio};
+
+type Stdout = BufReader<ChildStdout>;
 
 const SERVER_JAR_PATH: &str =
     "/Users/npc/projects/mine/mc-server-wrapper/server-playground/server.jar";
@@ -18,18 +20,8 @@ fn main() -> Result<(), Box<dyn error::Error>> {
             .stdout
             .ok_or_else(|| Error::new(ErrorKind::Other, "could not capture stdout"))?,
     );
-    let mut buf = String::new();
 
-    while !buf.contains("Done") {
-        print!("{}", &buf);
-        buf.clear();
-        stdout.read_line(&mut buf)?;
-    }
-    // Print and clear the buffer one last time. At this point, its content are
-    // the "Done" line.
-    print!("{}", &buf);
-    buf.clear();
-
+    wait_for_server_to_spin_up(&mut stdout)?;
     println!("Reached the point where the server is up");
 
     child
@@ -37,13 +29,28 @@ fn main() -> Result<(), Box<dyn error::Error>> {
         .as_mut()
         .ok_or("could not capture stdin")?
         .write_all(b"/stop\n")?;
-
     println!("Just sent the stop command");
 
     stdout
         .lines()
         .filter_map(|line| line.ok())
         .for_each(|line| println!("{}", line));
+
+    Ok(())
+}
+
+fn wait_for_server_to_spin_up(stdout: &mut Stdout) -> Result<(), Error> {
+    let mut buf = String::new();
+    while !buf.contains("Done") {
+        // TODO: Temporary.
+        print!("{}", &buf);
+        buf.clear();
+        stdout.read_line(&mut buf)?;
+    }
+    // Print and clear the buffer one last time. At this point, its contents are
+    // the "Done" line.
+    print!("{}", &buf);
+    buf.clear();
 
     Ok(())
 }
