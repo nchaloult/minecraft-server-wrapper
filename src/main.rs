74def56c2c57252b336a473b416764d90c122a4b
Nick Chaloult <npchaloult@gmail.com>
2022-01-13 01:52:55 -0500

    Add clarifying comments for the logic in main()

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,27 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn error::Error>> {
+    // Get a new server wrapper, and wait for that wrapper to launch the
+    // underlying Minecraft server.
+    //
+    // The wrapper is guarded by a mutex because it's not designed to be used
+    // asynchronously, but a shared reference of it is given to each HTTP API
+    // handler.
+    //
+    // That whole thing is wrapped in an Arc so we can share ownership of that
+    // mutex across multiple async tasks, and consequently multiple threads.
     let wrapper = Arc::new(Mutex::new(Wrapper::new()?));
     wrapper.lock().unwrap().wait_for_server_to_spin_up()?;
 
+    // Get a one-time-use channel that will carry a message indicating that the
+    // warp server should be shut down. Designed to be used by the handler for
+    // the /stop route -- this way, when the Minecraft server spins down, we'll
+    // stop serving new incoming requests to talk to it.
     let (shutdown_signal_tx, shutdown_signal_rx) = sync::oneshot::channel::<()>();
+    // Wrapped in an Arc<Mutex<_>> for the same reasons as the server wrapper.
     let shutdown_signal_tx_mutex = Arc::new(Mutex::new(Some(shutdown_signal_tx)));
 
+    // Route filters.
     let stop_server = warp::path("stop")
         .and(warp::path::end())
         .and(warp::get())
@@ -32,7 +47,10 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
         .and(with_wrapper(wrapper.clone()))
         .and_then(handlers::list_players);
 
-    // Passing stdin to the wrapper's stdin
+    // Pass any lines that are written to stdin onto the underlying Minecraft
+    // server's stdin pipe. This lets server admins with access to the machine
+    // that the server is running on interact with it the same way they would if
+    // this wrapper weren't present.
     let stdin_reader = io::BufReader::new(io::stdin());
     thread::spawn(move || {
         stdin_reader
@@ -46,7 +64,7 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
             });
     });
 
-    // API server
+    // Stand up the API server.
     let routes = stop_server.or(list_players);
     let (_, server) =
         warp::serve(routes).bind_with_graceful_shutdown(([0, 0, 0, 0], 6969), async {
